8f5079f4bfbbf8a51ac78608b8b4b9fcfbd15b55
thinh truong <thinhtruong2305@gmail.com>
2021-12-13 12:11:34 +0700

    Hoàn thiện Statistical_BUS lần 1 commit

---
+++ PetShopWinform/BUS/Statistical_BUS.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetShopWinform.DAO;
+using System.Windows.Forms;
+
+namespace PetShopWinform.BUS
+{
+    class Statistical_BUS
+    {
+        private Statistical_DAO danhSachThongKe;
+
+        public Statistical_BUS() { danhSachThongKe = new Statistical_DAO(); }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="view"></param>
+        public void layDanhSachThongKe(DataGridView view)
+        {
+            view.DataSource = danhSachThongKe.layDanhSachThongKe();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="view"></param>
+        /// <param name="ngayBatDau"></param>
+        /// <param name="ngayKetThuc"></param>
+        public void layDanhSachThongKeTheoNgay(DataGridView view, DateTime ngayBatDau, DateTime ngayKetThuc)
+        {
+            view.DataSource = danhSachThongKe.locDanhSachTheoNgay(ngayBatDau, ngayKetThuc);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="view"></param>
+        /// <param name="tuKhoa"></param>
+        public void timHoaDon(DataGridView view, String tuKhoa)
+        {
+            view.DataSource = danhSachThongKe.timKiemDanhSach(tuKhoa);
+        }
+    }
+}
